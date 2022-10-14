185a15e237547851c64e8b1d0a3b2b95bf8ba2f4
AY-KE-4519 <kuberimk@gmail.com>
2022-10-15 00:48:46 +0530

    Added Navbar Hover-collapsing

--- js/index.js
+++ js/index.js
@@ -1,21 +1,60 @@
 const pageDisplay = document.querySelector("#pageDisplay");
-const homeBtn = document.querySelector("#homeBtn");
-const aboutBtn = document.querySelector("#aboutBtn");
-const lanternsBtn = document.querySelector("#lanternsBtn");
-const earthernlampsBtn = document.querySelector("#earthernlampsBtn");
-const offersBtn = document.querySelector("#offersBtn");
-homeBtn.addEventListener("click", () => {
-  pageDisplay.src = "home.html";
+const homeBtns = document.querySelectorAll(".homeBtn");
+const aboutBtns = document.querySelectorAll(".aboutBtn");
+const lanternsBtns = document.querySelectorAll(".lanternsBtn");
+const earthernlampsBtns = document.querySelectorAll(".earthernlampsBtn");
+const offersBtns = document.querySelectorAll(".offersBtn");
+homeBtns.forEach((homeBtn) => {
+  homeBtn.addEventListener("click", () => {
+    pageDisplay.src = "home.html";
+  });
 });
-aboutBtn.addEventListener("click", () => {
-  pageDisplay.src = "about.html";
+aboutBtns.forEach((aboutBtn) => {
+  aboutBtn.addEventListener("click", () => {
+    pageDisplay.src = "about.html";
+  });
 });
-lanternsBtn.addEventListener("click", () => {
-  pageDisplay.src = "lanterns.html";
+lanternsBtns.forEach((lanternsBtn) => {
+  lanternsBtn.addEventListener("click", () => {
+    pageDisplay.src = "lanterns.html";
+  });
 });
-earthernlampsBtn.addEventListener("click", () => {
-  pageDisplay.src = "earthern_lamps.html";
+earthernlampsBtns.forEach((earthernlampsBtn) => {
+  earthernlampsBtn.addEventListener("click", () => {
+    pageDisplay.src = "earthern_lamps.html";
+  });
 });
-offersBtn.addEventListener("click", () => {
-  pageDisplay.src = "offers.html";
+offersBtns.forEach((offersBtn) => {
+  offersBtn.addEventListener("click", () => {
+    pageDisplay.src = "offers.html";
+  });
+});
+
+const navBtns = document.querySelectorAll(".navBtns");
+const sidebarlogos = document.querySelectorAll(".sidebarlogo");
+sidebarlogos.forEach((sidebarlogo) => {
+  sidebarlogo.addEventListener("mouseover", () => {
+    navBtns.forEach((navBtn) => {
+      navBtn.style.display = "table-cell";
+    });
+    sidebarlogo.parentNode.style.backgroundColor = "#c80";
+    sidebarlogo.parentNode.nextElementSibling.style.backgroundColor = "#c80";
+  });
+  sidebarlogo.addEventListener("mouseout", () => {
+    sidebarlogo.parentNode.style.backgroundColor = "#fa0";
+    sidebarlogo.parentNode.nextElementSibling.style.backgroundColor = "#fa0";
+  });
+  navBtns.forEach((navBtn) => {
+    navBtn.addEventListener("mouseover", () => {
+      navBtn.style.backgroundColor = "#c80";
+      navBtn.previousElementSibling.style.backgroundColor = "#c80";
+    });
+    navBtn.addEventListener("mouseout", () => {
+      navBtn.style.backgroundColor = "#fa0";
+      navBtn.previousElementSibling.style.backgroundColor = "#fa0";
+      navBtns.forEach((navBtn) => {
+        navBtn.style.display = "none";
+      });
+    });
+  });
 });
