d282ad4c4cb2980f969ebeb53f7017dcca7b28b8
AY-KE-4519 <kuberimk@gmail.com>
2022-10-16 21:38:38 +0530

    sorted by price

--- js/lanterns.js
+++ js/lanterns.js
@@ -1,22 +1,22 @@
 const cardGrid = document.querySelector(".cardGrid");
 const lanternPricePairs = [
+  { code: "A1KG_PURPLE", price: 200 },
+  { code: "A1KG_WHITE", price: 200 },
   { code: "A1K_BLUE", price: 250 },
   { code: "A1K_LBLUE", price: 250 },
   { code: "A1K_ORANGE", price: 250 },
   { code: "A1K_PINK", price: 250 },
   { code: "A1K_WHITE", price: 250 },
   { code: "A1K_YELLOW", price: 250 },
-  { code: "A1KG_PURPLE", price: 200 },
-  { code: "A1KG_WHITE", price: 200 },
   { code: "A1DK", price: 250 },
   { code: "A2DK", price: 280 },
   { code: "A1F", price: 280 },
-  { code: "PJ1", price: 400 },
-  { code: "PJ2", price: 500 },
   { code: "A1P_1", price: 280 },
   { code: "A1P_2", price: 280 },
   { code: "A1P_3", price: 280 },
   { code: "A1KV", price: 300 },
+  { code: "PJ1", price: 400 },
+  { code: "PJ2", price: 500 },
 ];
 
 lanternPricePairs.forEach((lanternPricePair) => {
