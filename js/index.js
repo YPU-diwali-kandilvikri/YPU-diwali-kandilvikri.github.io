e7f0680b24d8d9c07b70032b84dfef774c6cf7b6
AY-KE-4519 <kuberimk@gmail.com>
2022-10-16 11:04:17 +0530

    added lantern codes and prices

--- js/index.js
+++ js/index.js
@@ -7,29 +7,40 @@ const offersBtns = document.querySelectorAll(".offersBtn");
 const navBtns = document.querySelectorAll(".navBtns");
 const sidebarlogos = document.querySelectorAll(".sidebarlogo");
 
+if (sessionStorage.getItem("page") != undefined) {
+  pageDisplay.src = sessionStorage.getItem("page");
+} else {
+  pageDisplay.src = "home.html";
+}
+
 homeBtns.forEach((homeBtn) => {
   homeBtn.addEventListener("click", () => {
     pageDisplay.src = "home.html";
+    sessionStorage.setItem("page", "home.html");
   });
 });
 aboutBtns.forEach((aboutBtn) => {
   aboutBtn.addEventListener("click", () => {
     pageDisplay.src = "about.html";
+    sessionStorage.setItem("page", "about.html");
   });
 });
 lanternsBtns.forEach((lanternsBtn) => {
   lanternsBtn.addEventListener("click", () => {
     pageDisplay.src = "lanterns.html";
+    sessionStorage.setItem("page", "lanterns.html");
   });
 });
 earthernlampsBtns.forEach((earthernlampsBtn) => {
   earthernlampsBtn.addEventListener("click", () => {
     pageDisplay.src = "earthern_lamps.html";
+    sessionStorage.setItem("page", "earthern_lamps.html");
   });
 });
 offersBtns.forEach((offersBtn) => {
   offersBtn.addEventListener("click", () => {
     pageDisplay.src = "offers.html";
+    sessionStorage.setItem("page", "offers.html");
   });
 });
 
--- js/lanterns.js
+++ js/lanterns.js
@@ -1,3 +1,45 @@
+const lanternsTable = document.querySelector("#lanterns");
+const lanternPricePairs = [
+  { code: "A1K_BLUE", price: 250 },
+  { code: "A1K_LBLUE", price: 250 },
+  { code: "A1K_ORANGE", price: 250 },
+  { code: "A1K_PINK", price: 250 },
+  { code: "A1K_WHITE", price: 250 },
+  { code: "A1K_YELLOW", price: 250 },
+  { code: "A1KG_PURPLE", price: 200 },
+  { code: "A1KG_WHITE", price: 200 },
+  { code: "A1DK", price: 250 },
+  { code: "A2DK", price: 280 },
+  { code: "A1F", price: 280 },
+  { code: "PJ1", price: 400 },
+  { code: "PJ2", price: 500 },
+  { code: "A1P_1", price: 280 },
+  { code: "A1P_2", price: 280 },
+  { code: "A1P_3", price: 280 },
+  { code: "A1KV", price: 300 },
+];
+
+for (let z = 0; z < 6; z++) {
+  const row = document.createElement("tr");
+  for (let i = 0; i < 3; i++) {
+    if (lanternPricePairs[i]) {
+      const cell = document.createElement("td");
+      const lantern = document.createElement("lantern");
+      let lantern_code = lanternPricePairs[i].code;
+      let price = lanternPricePairs[i].price;
+      lantern.setAttribute("img", `./images/${lantern_code}.jpg`);
+      lantern.setAttribute("name", lantern_code);
+      lantern.setAttribute("price", price);
+      row.appendChild(cell);
+      cell.appendChild(lantern);
+    } else {
+      break;
+    }
+  }
+  lanternPricePairs.splice(0, 3);
+  lanternsTable.appendChild(row);
+}
+
 const lanterns = document.querySelectorAll("lantern");
 lanterns.forEach((lantern) => {
   let imageUrl = lantern.getAttribute("img");
@@ -6,7 +48,7 @@ lanterns.forEach((lantern) => {
   let componentCode = /*html*/ `
 <div class="card">
     <a href="${imageUrl}" target="_blank"><img class="lantern-img" src="${imageUrl}" alt=""></img></a><br/>
-    <span class="card-title text-glow">${name} - ${price}</span>
+    <span class="card-title text-glow">${name} - Rs. ${price}</span>
 </div>
     `;
   lantern.innerHTML = componentCode;
